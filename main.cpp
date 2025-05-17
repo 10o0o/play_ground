010954613d5254130056ba723fab9a409db264d0
10o0o <wotjs2923@gmail.com>
2025-05-17 20:20:24 +0900

    feat: cpp settings

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO                 \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);            \
+  cout.tie(nullptr);
+
+#define FOR(i, a, b) for (int i = a; i < b; i += 1)
+#define FRE(i, b, a) for (int i = b; i >= a; i -= 1)
+#define FOF(it, vec) for (auto& it : vec)
+#define endl '\n'
+#define ALL(v) v.begin(), v.end()
+#define DEBUG_VAR(var) cout << #var << ": " << (var) << endl;
+// #define DEBUG
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+int main() {
+  FASTIO;
+
+  cout << "hi" << endl;
+
+  return 0;
+}
